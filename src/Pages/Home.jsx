124d52e9e5d7cfcbe2381f0e0b481d0e7d435580
JoelNieto <joenieto90@hotmail.com>
2020-10-23 15:13:29 -0500

    Create Component InfoAlbum

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,19 @@ import React, { Component }  from 'react';
 
 import Panel from '../Pages/Panel';
 import '../styles/PagesStyles/Home.scss';
-import CircleAlbumGarden from '../components/CirculeAlbumGarden'
+import CircleAlbumGarden from '../components/CirculeAlbumGarden';
+import InfoAlbum from '../components/InfoAlbum';
+import AlbumArtist from '../Assets/AlbumArtist.PNG';
 
 export default class Home extends Component {
     render() {
         return(
             <div className='home'>
                 <Panel />
-                <CircleAlbumGarden SectionName='Section Name'/>
+                <section>
+                    <CircleAlbumGarden SectionName='Section Name'/>
+                    <InfoAlbum imgAlbum={AlbumArtist} NameAlbum='Illisions' Artist='Ibrahim Maalouf' Description='The artists we represent are one of the most successful in Romania and also were a huge breakthrough in the international market, topping radio and sales around the world.' />
+                </section>
             </div>
         )
     }
