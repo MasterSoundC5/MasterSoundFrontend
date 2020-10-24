26dc1098657675f989db0cebe5f181d083cdc71a
Hector Remedios <hectoremedio90@gmail.com>
2020-10-24 04:30:28 -0500

    animation of logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
 import Register from './Pages/Register';
+import AnimationLoader from './Pages/AnimationLoader'
 
 function App() {
   return (
@@ -17,6 +18,11 @@ function App() {
             exact 
             path='/signin' 
             component={SignIn} 
+          />
+          <Route 
+            exact 
+            path='/animation' 
+            component={AnimationLoader} 
           />
           <Route 
             exact 
---
+++ src/Pages/AnimationLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import LogoAnimado from '../components/LogoAnimado'
+
+const AnimationLoader = () => {
+    return(
+        <LogoAnimado />
+    )
+}
+
+export default AnimationLoader;
