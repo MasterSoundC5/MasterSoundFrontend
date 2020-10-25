3b76edcdc4093852b6a7ba1a629a004f27289aea
Hector Remedios <hectoremedio90@gmail.com>
2020-10-24 21:54:51 -0500

    correction in home css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,15 +11,15 @@ import SquareAlbumGarden from '../components/SquareAlbumGarden'
 export default class Home extends Component {
     render() {
         return(
-            <div className='home'>
-                <Panel />
-                <section>
-                    <CircleAlbumGarden SectionName='Section Name'/>
-                    <SquareAlbumGarden SectionName='Tendencia'/>
-                    <PurpleButtonGarden />
-                    <InfoAlbum imgAlbum={AlbumArtist} NameAlbum='Illisions' Artist='Ibrahim Maalouf' Description='The artists we represent are one of the most successful in Romania and also were a huge breakthrough in the international market, topping radio and sales around the world.' />
-                </section>
-            </div>
+            <div className='Home__container'>
+                    <Panel />
+                    <section className='Content__section'>
+                        <CircleAlbumGarden SectionName='Section Name'/>
+                        <SquareAlbumGarden SectionName='Tendencia'/>
+                        <PurpleButtonGarden />
+                        <InfoAlbum imgAlbum={AlbumArtist} NameAlbum='Illisions' Artist='Ibrahim Maalouf' Description='The artists we represent are one of the most successful in Romania and also were a huge breakthrough in the international market, topping radio and sales around the world.' />
+                    </section>
+                </div>
         )
     }
 }
