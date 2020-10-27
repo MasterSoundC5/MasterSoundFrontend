e786de97bd429696c044d91b0c20c9f89950f415
JoelNieto <joenieto90@hotmail.com>
2020-10-26 18:22:07 -0600

    Create component Top Tracks

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Panel from '../components/Panel';
 import '../styles/PagesStyles/Home.scss';
 import CircleAlbumGarden from '../components/CirculeAlbumGarden';
 import InfoAlbum from '../components/InfoAlbum';
+import TopTracks from '../components/TopTracks';
 import AlbumArtist from '../Assets/AlbumArtist.PNG';
 import PurpleButtonGarden from '../components/PurpleButtonGarden'
 import SquareAlbumGarden from '../components/SquareAlbumGarden'
@@ -21,6 +22,7 @@ export default class Home extends Component {
                         <PurpleButtonGarden />
                         <RectangleAlbumGarden SectionName='Tendencia'/>
                         <InfoAlbum imgAlbum={AlbumArtist} NameAlbum='Illisions' Artist='Ibrahim Maalouf' Description='The artists we represent are one of the most successful in Romania and also were a huge breakthrough in the international market, topping radio and sales around the world.' />
+                        <TopTracks />
                     </section>
                     
                 </div>
