0d26fb0846fd3f48c25f76d84672186833c8688e
Hector Remedios <hectoremedio90@gmail.com>
2020-10-30 15:16:13 -0600

    Starting the audio player

--- src/components/InfoAlbum.jsx
+++ src/components/InfoAlbum.jsx
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 import ListAlbum from './ListAlbum';
-import MySpotifyPlayer from './MySpotifyPlayer';
+//import MySpotifyPlayer from './MySpotifyPlayer';
 import '../styles/componentsStyles/InfoAlbum.scss';
 
 
 // size may also be a plain string using the presets 'large' or 'compact'
-const size = {
+/* const size = {
     width: '100%',
     height: 300,
   };
   const view = 'list'; // or 'coverart'
-  const theme = 'white'; // or 'white'
+  const theme = 'black'; // or 'white' */
 
 
 function InfoAlbum({ data }) {
@@ -20,7 +20,7 @@ function InfoAlbum({ data }) {
     })
 //----------------------getting songs from local storage-----------------------//
     const musicLocalstorage = JSON.parse( localStorage.getItem("SongsList"))
-    const idAlbum = data.spt_album_id;
+    //const idAlbum = data.spt_album_id;
     
     return (
         <Fragment>
@@ -31,12 +31,13 @@ function InfoAlbum({ data }) {
                     <p className='InfoAlbum__Subtitle--NameAlbum'>{data.album_name}</p>
                     <p className='InfoAlbum__Subtitle--Artist'>{nameArtists}</p>
                     <p className='InfoAlbum__Subtitle--Description'>{data.name}</p>
-                    <MySpotifyPlayer 
+                    
+                    {/* <MySpotifyPlayer 
                         uri= {`spotify:album:${idAlbum}`}
                         size={size}
                         view={view}
                         theme={theme} 
-                    />
+                    /> */}
                         {
                             musicLocalstorage.map( (item) => (
                                 <ListAlbum data={item} {...item}/>
