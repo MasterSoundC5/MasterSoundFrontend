bbd90cd168171e2dab764563e4c819cc7f273d46
Alvaro Arturo <alvaroj.arturo@gmail.com>
2020-10-31 14:19:31 -0500

    Add search icon

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Bell from '../components/Icons/Bell'
 import Radio from '../components/Icons/Radio'
 import Tv from '../components/Icons/Tv'
-import SearchIcon from '../Assets/Icons/Search.svg'
+
 
 import '../styles/componentsStyles/MenuBar.scss'
 
@@ -10,7 +10,6 @@ const MenuBar = () => {
   return (
 	<nav class="nav-wrapper-flex">
 		<input type="text" placeholder="Search your entertainment"></input>
-    <i href={SearchIcon} className="Input__icon" aria-hidden="true"></i>
 		<ul class="nav-links">
 			<li><a href="#"><Tv /></a></li>
 			<li><a href="#"><Radio /></a></li>
