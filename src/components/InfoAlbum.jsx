8d040b1afeba410cfefa58c50276aeb29cbf7908
Hector Remedios <hectoremedio90@gmail.com>
2020-10-31 16:51:17 -0600

    info album correction

--- src/components/InfoAlbum.jsx
+++ src/components/InfoAlbum.jsx
@@ -1,16 +1,6 @@
 import React, { Fragment } from 'react';
 import ListAlbum from './ListAlbum';
-//import MySpotifyPlayer from './MySpotifyPlayer';
 import '../styles/componentsStyles/InfoAlbum.scss';
-
-
-// size may also be a plain string using the presets 'large' or 'compact'
-/* const size = {
-    width: '100%',
-    height: 300,
-  };
-  const view = 'list'; // or 'coverart'
-  const theme = 'black'; // or 'white' */
 
 
 function InfoAlbum({ data }) {
@@ -20,7 +10,6 @@ function InfoAlbum({ data }) {
     })
 //----------------------getting songs from local storage-----------------------//
     const musicLocalstorage = JSON.parse( localStorage.getItem("SongsList"))
-    //const idAlbum = data.spt_album_id;
     
     return (
         <Fragment>
@@ -31,13 +20,6 @@ function InfoAlbum({ data }) {
                     <p className='InfoAlbum__Subtitle--NameAlbum'>{data.album_name}</p>
                     <p className='InfoAlbum__Subtitle--Artist'>{nameArtists}</p>
                     <p className='InfoAlbum__Subtitle--Description'>{data.name}</p>
-                    
-                    {/* <MySpotifyPlayer 
-                        uri= {`spotify:album:${idAlbum}`}
-                        size={size}
-                        view={view}
-                        theme={theme} 
-                    /> */}
                         {
                             musicLocalstorage.map( (item) => (
                                 <ListAlbum data={item} {...item}/>
