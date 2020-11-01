f446ec1a8adc6e7cef8d100550a8f4b39d5b75b9
Alvaro Arturo <alvaroj.arturo@gmail.com>
2020-11-01 03:09:29 -0500

    SearchFilter component init

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,6 +3,8 @@ import Bell from '../components/Icons/Bell'
 import Radio from '../components/Icons/Radio'
 import Tv from '../components/Icons/Tv'
 
+import SearchFilter from '../components/SearchFilter'
+
 
 import '../styles/componentsStyles/MenuBar.scss'
 import UserProfile from './UserProfile'
@@ -10,7 +12,7 @@ import UserProfile from './UserProfile'
 const MenuBar = () => {
   return (
 	<nav className="nav-wrapper-flex">
-		<input type="text" placeholder="Search your entertainment"></input>
+		<SearchFilter />
 		<ul className="nav-links">
 			<li className="Tv__nav"><a href="/"><Tv /></a></li>
 			<li><a href="/"><Radio /></a></li>
---
+++ src/components/SearchFilter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchFilter = () => {
+  return (
+    <input type="text" placeholder="Search your entertainment" />
+  )
+}
+
+export default SearchFilter
