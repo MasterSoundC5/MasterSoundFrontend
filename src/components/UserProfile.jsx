8ebef53ae4c5e535933df1a3bcfb753ba6d5be5f
Alvaro Arturo <alvaroj.arturo@gmail.com>
2020-11-01 02:51:08 -0500

    Gravatar init

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+
+import { connect } from 'react-redux'
+import Gravatar from '../components/Gravatar'
+
 import UserImage from '../Assets/ProfileDefaultImage.png'
 import '../styles/componentsStyles/UserProfile.scss'
 
 
-const UserProfile = ({ UserName }) => {
+
+const UserProfile = ({ UserName }, props) => {
+  const { user } = props
+  const hashUser = Object.keys(user).length > 0
   return (
     <section className="UserProfile__section">
-      <img src={UserImage} alt=""/>
+      {hashUser ?
+        <img src={Gravatar(user.email)} alt={user.email} /> :
+        <img src={UserImage} alt=""/>
+      }
       <p>AF Shichan {UserName}</p>
     </section>
   )
 }
 
-export default UserProfile
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(UserProfile)
